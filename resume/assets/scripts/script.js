e4c58a665dccc43bf46d1b343aa6ddbe62e4f08c
Greg Hines <greg@greghines.com>
2019-08-11 22:10:43 -0600

    Inital version.

---
+++ resume/assets/scripts/script.js
@@ -0,0 +1,13 @@
+// Create buttons.
+    // <div id="print-paper" class="print-button"><img src="assets/icons/printer.svg" alt="Printer"></div>
+    // <div id="print-pdf" class="print-button"><img src="assets/icons/pdf.svg" alt="PDF"></div>
+
+// Print (PDF) Button
+    // Disable the Paper stylesheet, if active.
+    // Switch icons to the white icons.
+    // Bring up the print dialog.
+
+// Print (Paper) Button
+    // Disable the PDF stylesheet, if active.
+    // Switch icons to the black icons.
+    // Bring up the print dialog.
